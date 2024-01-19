13e8361e3bdc0e0e971c2bb753463790caffc5a1
TheoMa76 <tmaerten@normandiewebschool.fr>
2024-01-19 16:52:16 +0100

    supprimer le js de bootstrap

--- templates/includes/html_header.inc.php
+++ templates/includes/html_header.inc.php
@@ -4,7 +4,6 @@
   <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="styles/global.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 </head>
 <body>
 
